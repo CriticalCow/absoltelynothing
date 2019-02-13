a16c09778e3cf98e12e906f28a0e07ebada0af68
CriticalCow <37348897+CriticalCow@users.noreply.github.com>
2019-02-13 07:17:59 -0800

    Add files via upload

---
+++ gaytest.py
@@ -0,0 +1,20 @@
+import time
+
+print("Welcome to the gay test! There will be 20 questions to determine your gayness")
+time.sleep(2)
+name = input("Enter your name for better gay accuracy reading: ")
+print("Ok hello " + name + "!")
+time.sleep(1)
+choice_1 = input("Ok next question, which company products do you like more? Apple/Google ")
+print("Apple or Google")
+if choice_1 == "Google" or "google" or "android" or "Andorid":print("Ok, i havent gotten any gay readings on you yet.")
+if choice_1 == "Apple" or "apple":print("Wow, ok um.. nothing to worry about just got a massive spike on the gayness meter.. haha " + name + " likes apple products")
+time.sleep(1.5)
+print("next question!")
+time.sleep(.5)
+choice_2 = input("is your name f0ur? y/n ")
+if choice_2 = "No" or "no" or "n" or "N":print("ok your good then")
+if choice_2 = "Yes" or "yes" or "Y" or "y":print("your as gay as can be")
+
+
+
